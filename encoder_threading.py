79f5634342a5edc1ef8b7d791698a028bbab5df5
samaco634 <72288990+samaco634@users.noreply.github.com>
2023-03-02 11:02:12 +0900

    Update encoder_threading.py

--- encoder_threading.py
+++ encoder_threading.py
@@ -15,51 +15,52 @@ class EncoderThread(threading.Thread):
         self.start()
 
     def run(self):
-        #check GPIO
-        p1 = GPIO.input(self.leftPin)
-        p2 = GPIO.input(self.rightPin)
-        newState = "{}{}".format(p1, p2)
+        while self.running :
+            #check GPIO
+            p1 = GPIO.input(self.leftPin)
+            p2 = GPIO.input(self.rightPin)
+            newState = "{}{}".format(p1, p2)
 
-        if self.state == "00": # Resting position
-            if newState == "01": # Turned right 1
-                self.direction = "R"
-            elif newState == "10": # Turned left 1
-                self.direction = "L"
+            if self.state == "00": # Resting position
+                if newState == "01": # Turned right 1
+                    self.direction = "R"
+                elif newState == "10": # Turned left 1
+                    self.direction = "L"
 
-        elif self.state == "01": # R1 or L3 position
-            if newState == "11": # Turned right 1
-                self.direction = "R"
-            elif newState == "00": # Turned left 1
-                if self.direction == "L":
-                    self.value = self.value - 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
+            elif self.state == "01": # R1 or L3 position
+                if newState == "11": # Turned right 1
+                    self.direction = "R"
+                elif newState == "00": # Turned left 1
+                    if self.direction == "L":
+                        self.value = self.value - 1
+                        if self.callback is not None:
+                            self.callback(self.value, self.direction)
 
-        elif self.state == "10": # R3 or L1
-            if newState == "11": # Turned left 1
-                self.direction = "L"
-            elif newState == "00": # Turned right 1
-                if self.direction == "R":
-                    self.value = self.value + 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
+            elif self.state == "10": # R3 or L1
+                if newState == "11": # Turned left 1
+                    self.direction = "L"
+                elif newState == "00": # Turned right 1
+                    if self.direction == "R":
+                        self.value = self.value + 1
+                        if self.callback is not None:
+                            self.callback(self.value, self.direction)
 
-        else: # self.state == "11"
-            if newState == "01": # Turned left 1
-                self.direction = "L"
-            elif newState == "10": # Turned right 1
-                self.direction = "R"
-            elif newState == "00": # Skipped an intermediate 01 or 10 state, but if we know direction then a turn is complete
-                if self.direction == "L":
-                    self.value = self.value - 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
-                elif self.direction == "R":
-                    self.value = self.value + 1
-                    if self.callback is not None:
-                        self.callback(self.value, self.direction)
-                
-        self.state = newState
+            else: # self.state == "11"
+                if newState == "01": # Turned left 1
+                    self.direction = "L"
+                elif newState == "10": # Turned right 1
+                    self.direction = "R"
+                elif newState == "00": # Skipped an intermediate 01 or 10 state, but if we know direction then a turn is complete
+                    if self.direction == "L":
+                        self.value = self.value - 1
+                        if self.callback is not None:
+                            self.callback(self.value, self.direction)
+                    elif self.direction == "R":
+                        self.value = self.value + 1
+                        if self.callback is not None:
+                            self.callback(self.value, self.direction)
+
+            self.state = newState
  
     def terminate(self):
         print('terminating thread')
